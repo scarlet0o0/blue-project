18554a4970563c05d4ee59ed376f20ec04fb0644
scarlet0o0 <ghd7673@naver.com>
2024-02-05 21:34:24 +0900

    Feat : @Override 선언

--- src/main/java/com/rainbow/blue/repository/mapper/PostMapper.java
+++ src/main/java/com/rainbow/blue/repository/mapper/PostMapper.java
@@ -1,9 +1,25 @@
 package com.rainbow.blue.repository.mapper;
 
+import com.rainbow.blue.domain.Post;
 import com.rainbow.blue.repository.PostRepository;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PostMapper extends PostRepository {
 
+    @Override
+    List<Post> AllPosts();
+
+    @Override
+    Post getPostById(Long id);
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    void save(Post post);
+
+    @Override
+    void update(Post post);
 }
