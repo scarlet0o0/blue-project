4dc8b913f3ccc21f7b4be08aaace2a5243ba3898
scarlet0o0 <ghd7673@naver.com>
2024-02-13 22:22:48 +0900

    Feat : 가독성 좋게 수정

--- src/main/java/com/rainbow/blue/error/GlobalExceptionHandler.java
+++ src/main/java/com/rainbow/blue/error/GlobalExceptionHandler.java
@@ -1,15 +1,12 @@
 package com.rainbow.blue.error;
 
-import org.springframework.http.HttpStatus;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,8 +14,8 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<String> errorMessages = e.getBindingResult().getAllErrors().stream()
                 .map(ObjectError::getDefaultMessage)
-                .collect(toList());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessages);
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errorMessages);
     }
 
 }
